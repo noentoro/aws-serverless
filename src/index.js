085496c3a3ca8e4b46044152332fa1c9e8aeee24
noentoro <nicholasoentoro@gmail.com>
2021-02-26 17:00:22 -0800

    clone https://github.com/PacktPublishing/Serverless-React-with-AWS-Amplify---The-Complete-Guide

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Bring in default Element React theme
+import "element-theme-default";
+
+Amplify.configure(aws_exports);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// reportWebVitals();
+serviceWorker.unregister();
